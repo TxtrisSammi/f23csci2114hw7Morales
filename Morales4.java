dc31a7ce0f1e694bed7c9fc9f0560a8c291e8558
Samantha Morales <samanthamp43@gmail.com>
2025-11-25 17:09:36 -0600

    complete Morales4

---
+++ Morales4.java
@@ -0,0 +1,30 @@
+// 906609
+public class Morales4 {
+    public static void main(String[] args) {
+        int bigPal = 0;
+        for (int i = 999; i > 0; i--) {
+            for (int j = 999; j > 0; j--) {
+                int product = i * j;
+                if (isPalindrome(product) && product > bigPal) {
+                    bigPal = product;
+                    
+                }
+            }
+        }
+        System.out.println(bigPal);
+    }
+
+    public static boolean isPalindrome(int num) {
+        boolean p = true;
+        char[] array = String.valueOf(num).toCharArray();
+        int endIndex = array.length - 1;
+        for (int i = 0; i < array.length / 2; i++) {
+            if (array[i] != array[endIndex]) {
+                p = false;
+                break;
+            }
+            endIndex --;
+        }
+        return p;
+    }
+}
